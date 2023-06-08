7efe6f916f68d1ee3d9a8752d8f98552c762812a
BOdantik <108508248+BOdantik@users.noreply.github.com>
2023-06-08 08:54:07 +0300

    NDVI-WFA

---
+++ vegwtation/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace vegwtation
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Start = new Button();
+            AVG_INDEX_TX_BOX = new TextBox();
+            label1 = new Label();
+            Wheat = new CheckBox();
+            Corn = new CheckBox();
+            Oat = new CheckBox();
+            Millet = new CheckBox();
+            Rice = new CheckBox();
+            Sunflower = new CheckBox();
+            textof = new Label();
+            Output_Label = new Label();
+            checkBoxSOYA = new CheckBox();
+            Picture_Box_Main1 = new PictureBox();
+            Calculation_Start = new Button();
+            ((System.ComponentModel.ISupportInitialize)Picture_Box_Main1).BeginInit();
+            SuspendLayout();
+            // 
+            // Start
+            // 
+            Start.Location = new Point(4, 417);
+            Start.Margin = new Padding(4, 3, 4, 3);
+            Start.Name = "Start";
+            Start.Size = new Size(192, 73);
+            Start.TabIndex = 0;
+            Start.Text = "Start";
+            Start.UseVisualStyleBackColor = true;
+            Start.Click += Start_Button;
+            // 
+            // AVG_INDEX_TX_BOX
+            // 
+            AVG_INDEX_TX_BOX.Location = new Point(12, 131);
+            AVG_INDEX_TX_BOX.Margin = new Padding(4, 3, 4, 3);
+            AVG_INDEX_TX_BOX.Name = "AVG_INDEX_TX_BOX";
+            AVG_INDEX_TX_BOX.Size = new Size(93, 25);
+            AVG_INDEX_TX_BOX.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Black", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label1.Location = new Point(12, 27);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(348, 37);
+            label1.TabIndex = 3;
+            label1.Text = "AVG VEGETATION INDEX";
+            // 
+            // Wheat
+            // 
+            Wheat.AutoSize = true;
+            Wheat.Location = new Point(12, 241);
+            Wheat.Margin = new Padding(4, 3, 4, 3);
+            Wheat.Name = "Wheat";
+            Wheat.Size = new Size(82, 21);
+            Wheat.TabIndex = 4;
+            Wheat.Text = "Пшениця";
+            Wheat.UseVisualStyleBackColor = true;
+            // 
+            // Corn
+            // 
+            Corn.AutoSize = true;
+            Corn.Location = new Point(12, 269);
+            Corn.Margin = new Padding(4, 3, 4, 3);
+            Corn.Name = "Corn";
+            Corn.Size = new Size(85, 21);
+            Corn.TabIndex = 5;
+            Corn.Text = "кукурудза";
+            Corn.UseVisualStyleBackColor = true;
+            // 
+            // Oat
+            // 
+            Oat.AutoSize = true;
+            Oat.Location = new Point(12, 297);
+            Oat.Margin = new Padding(4, 3, 4, 3);
+            Oat.Name = "Oat";
+            Oat.Size = new Size(57, 21);
+            Oat.TabIndex = 7;
+            Oat.Text = "Овес";
+            Oat.UseVisualStyleBackColor = true;
+            // 
+            // Millet
+            // 
+            Millet.AutoSize = true;
+            Millet.Location = new Point(12, 326);
+            Millet.Margin = new Padding(4, 3, 4, 3);
+            Millet.Name = "Millet";
+            Millet.Size = new Size(66, 21);
+            Millet.TabIndex = 8;
+            Millet.Text = "Просо";
+            Millet.UseVisualStyleBackColor = true;
+            // 
+            // Rice
+            // 
+            Rice.AutoSize = true;
+            Rice.Location = new Point(12, 354);
+            Rice.Margin = new Padding(4, 3, 4, 3);
+            Rice.Name = "Rice";
+            Rice.Size = new Size(47, 21);
+            Rice.TabIndex = 9;
+            Rice.Text = "Рис";
+            Rice.UseVisualStyleBackColor = true;
+            // 
+            // Sunflower
+            // 
+            Sunflower.AutoSize = true;
+            Sunflower.Location = new Point(12, 184);
+            Sunflower.Margin = new Padding(4, 3, 4, 3);
+            Sunflower.Name = "Sunflower";
+            Sunflower.Size = new Size(88, 21);
+            Sunflower.TabIndex = 11;
+            Sunflower.Text = "Соняшник";
+            Sunflower.UseVisualStyleBackColor = true;
+            // 
+            // textof
+            // 
+            textof.AutoSize = true;
+            textof.Location = new Point(144, 145);
+            textof.Margin = new Padding(4, 0, 4, 0);
+            textof.Name = "textof";
+            textof.Size = new Size(0, 17);
+            textof.TabIndex = 12;
+            // 
+            // Output_Label
+            // 
+            Output_Label.AutoSize = true;
+            Output_Label.Location = new Point(175, 148);
+            Output_Label.Margin = new Padding(4, 0, 4, 0);
+            Output_Label.Name = "Output_Label";
+            Output_Label.Size = new Size(0, 17);
+            Output_Label.TabIndex = 13;
+            // 
+            // checkBoxSOYA
+            // 
+            checkBoxSOYA.AutoSize = true;
+            checkBoxSOYA.Location = new Point(12, 211);
+            checkBoxSOYA.Margin = new Padding(4, 3, 4, 3);
+            checkBoxSOYA.Name = "checkBoxSOYA";
+            checkBoxSOYA.Size = new Size(50, 21);
+            checkBoxSOYA.TabIndex = 14;
+            checkBoxSOYA.Text = "Соя";
+            checkBoxSOYA.UseVisualStyleBackColor = true;
+            // 
+            // Picture_Box_Main1
+            // 
+            Picture_Box_Main1.BackColor = SystemColors.ControlDark;
+            Picture_Box_Main1.Dock = DockStyle.Right;
+            Picture_Box_Main1.Location = new Point(988, 0);
+            Picture_Box_Main1.Margin = new Padding(4, 3, 4, 3);
+            Picture_Box_Main1.Name = "Picture_Box_Main1";
+            Picture_Box_Main1.Size = new Size(916, 1041);
+            Picture_Box_Main1.SizeMode = PictureBoxSizeMode.AutoSize;
+            Picture_Box_Main1.TabIndex = 15;
+            Picture_Box_Main1.TabStop = false;
+            Picture_Box_Main1.Click += Picture_Box_Main1_Click;
+            Picture_Box_Main1.Paint += PictureBox1_Paint;
+            Picture_Box_Main1.MouseDown += Picture_Box_Main1_MouseDown;
+            Picture_Box_Main1.MouseUp += Picture_Box_Main1_MouseUp;
+            // 
+            // Calculation_Start
+            // 
+            Calculation_Start.Location = new Point(178, 534);
+            Calculation_Start.Margin = new Padding(4, 3, 4, 3);
+            Calculation_Start.Name = "Calculation_Start";
+            Calculation_Start.Size = new Size(75, 24);
+            Calculation_Start.TabIndex = 20;
+            Calculation_Start.Text = "Calculate";
+            Calculation_Start.UseVisualStyleBackColor = true;
+            Calculation_Start.Click += Calculation_Start_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(224, 224, 224);
+            ClientSize = new Size(1904, 1041);
+            Controls.Add(Calculation_Start);
+            Controls.Add(Picture_Box_Main1);
+            Controls.Add(checkBoxSOYA);
+            Controls.Add(Output_Label);
+            Controls.Add(textof);
+            Controls.Add(Sunflower);
+            Controls.Add(Rice);
+            Controls.Add(Millet);
+            Controls.Add(Oat);
+            Controls.Add(Corn);
+            Controls.Add(Wheat);
+            Controls.Add(label1);
+            Controls.Add(AVG_INDEX_TX_BOX);
+            Controls.Add(Start);
+            Cursor = Cursors.NoMove2D;
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)Picture_Box_Main1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+
+        #endregion
+
+        private Button Start;
+        private TextBox AVG_INDEX_TX_BOX;
+        private Label label1;
+        private CheckBox Wheat;
+        private CheckBox Corn;
+        private CheckBox soybeansCheckBox;
+        private CheckBox Oat;
+        private CheckBox Millet;
+        private CheckBox Rice;
+        private CheckBox Sunflower;
+        private Label textof;
+        private Label Output_Label;
+        private CheckBox checkBoxSOYA;
+        public PictureBox Picture_Box_Main1;
+        private Button Calculation_Start;
+
+    }
+}
---
+++ vegwtation/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+
+
+
+namespace vegwtation
+{
+    public partial class Form1 : Form
+    {
+        private Point Mouse_Down_Location;
+        private string Image_Path;
+        private Rectangle Selected_Area;
+        private bool Is_Selected_Area = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            Picture_Box_Main1.MouseDown += Picture_Box_Main1_MouseDown;
+            Picture_Box_Main1.MouseMove += Picture_Box_Main1_MouseMove;
+            Picture_Box_Main1.MouseUp += Picture_Box_Main1_MouseUp;
+        }
+
+        private void Start_Button(object sender, EventArgs e)
+        {
+            OpenFileDialog Open_File_Dialog = new OpenFileDialog();
+            Open_File_Dialog.Filter = "Images (*.bmp;*.jpg;*.png)|*.bmp;*.jpg;*.png";
+            DialogResult result = Open_File_Dialog.ShowDialog();
+
+            if (result != DialogResult.OK)
+            {
+                return;
+            }
+
+            Image_Path = Open_File_Dialog.FileName;
+
+            Picture_Box_Main1.Image = Image.FromFile(Image_Path);
+        }
+
+        private void All_Calculation(string Image_Path)
+        {
+            double Total_Ndvi_Val = 0, NDVI = 0, count = 0;
+            double Wheat_Rating_St = 0.8, Corn_Rating_St = 0.6, Soybeans_Rating_St = 0.9;
+            double Oats_Rating_St = 0.7;
+            double Millet_Rating_St = 0.5;
+            double Sunflower_Rating_St = 0.8;
+            double Rice_Rating_St = 0.6;
+            var Original_Image = new Bitmap(Image_Path);
+
+            if (Original_Image.PixelFormat != PixelFormat.Format24bppRgb)
+            {
+                Console.WriteLine("The image isn't in RGB format.\n");
+                Console.WriteLine("Converting the image to RGB format");
+
+                Bitmap New_Image = new Bitmap(Original_Image.Width, Original_Image.Height, PixelFormat.Format24bppRgb);
+                using (Graphics g = Graphics.FromImage(New_Image))
+                {
+                    g.DrawImage(Original_Image, 0, 0, Original_Image.Width, Original_Image.Height);
+                }
+
+                Original_Image = New_Image;
+            }
+
+          
+            bool Is_Wheat_Selected = Wheat.Checked;
+            bool Is_Corn_Selected = Corn.Checked;
+            bool Is_checkBox_SOYA_Selected = checkBoxSOYA.Checked;
+            bool Is_Oat_Selected = Oat.Checked;
+            bool Is_Millet_Selected = Millet.Checked;
+            bool Is_Sunflower_Selected = Sunflower.Checked;
+            bool Is_Rice_Selected = Rice.Checked;
+
+            if (!Selected_Area.IsEmpty)
+            {
+                for (int i = Selected_Area.Left; i < Selected_Area.Right; i++)
+                {
+                    for (int j = Selected_Area.Top; j < Selected_Area.Bottom; j++)
+                    {
+                        Color pixel = Original_Image.GetPixel(i, j);
+                        double red = (double)pixel.R / 255;
+                        double nir = (double)pixel.G / 255;
+
+                        NDVI = (double)(nir - red) / (nir + red);
+                        Total_Ndvi_Val += NDVI;
+                        count++;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < Original_Image.Width; i++)
+                {
+                    for (int j = 0; j < Original_Image.Height; j++)
+                    {
+                        Color pixel = Original_Image.GetPixel(i, j);
+                        double red = (double)pixel.R / 255;
+                        double nir = (double)pixel.G / 255;
+
+                        NDVI = (double)(nir - red) / (nir + red);
+                        Total_Ndvi_Val += NDVI;
+                        count++;
+                    }
+                }
+            }
+
+            double Avg_NDVI = Total_Ndvi_Val / count;
+            AVG_INDEX_TX_BOX.Text = Math.Round(Avg_NDVI, 3).ToString();
+
+            // Display crop type and rating based on average NDVI value
+            if (Is_Wheat_Selected && Avg_NDVI >= Wheat_Rating_St)
+            {
+                Output_Label.Text = "Wheat selected.Rating of the crop type is " + Wheat_Rating_St + ".";
+            }
+            else if (Is_Wheat_Selected && Avg_NDVI < Wheat_Rating_St)
+            {
+                Output_Label.Text = "Wheat selected. Rating of the crop type is " + Wheat_Rating_St + ". The crop type has a lower rating than the top rating  " + Math.Round((Wheat_Rating_St - Avg_NDVI) * 100, 2) + "%.";
+            }
+            if (Is_Corn_Selected && Avg_NDVI >= Corn_Rating_St)
+            {
+                Output_Label.Text = "Corn selected.Rating of the crop type is " + Corn_Rating_St + ".";
+            }
+            else if (Is_Corn_Selected && Avg_NDVI < Corn_Rating_St)
+            {
+                Output_Label.Text = "Corn selected.Rating of the crop type is " + Corn_Rating_St + ". The crop type has a lower rating than the top rating " + Math.Round((Corn_Rating_St - Avg_NDVI) * 100, 2) + "%.";
+            }
+            if (Is_checkBox_SOYA_Selected && Avg_NDVI >= Soybeans_Rating_St)
+            {
+                Output_Label.Text = "Soybeans selected.Rating of the crop type is " + Soybeans_Rating_St + ".";
+            }
+            else if (Is_checkBox_SOYA_Selected && Avg_NDVI < Soybeans_Rating_St)
+            {
+                Output_Label.Text = "Soybeans selected.Rating of the crop type is " + Soybeans_Rating_St + ". The crop type has a lower rating than the top rating  " + Math.Round((Soybeans_Rating_St - Avg_NDVI) * 100, 2) + "%.";
+            }
+            if (Is_Oat_Selected && Avg_NDVI >= Oats_Rating_St)
+            {
+                Output_Label.Text = "Oats selected.Rating of the crop type is " + Oats_Rating_St + ".";
+            }
+            else if (Is_Oat_Selected && Avg_NDVI < Oats_Rating_St)
+            {
+                Output_Label.Text = "Oats selected.Rating of the crop type is " + Oats_Rating_St + ". The crop type has a lower rating than the top rating  " + Math.Round((Oats_Rating_St - Avg_NDVI) * 100, 2) + "%.";
+            }
+            if (Is_Millet_Selected && Avg_NDVI >= Millet_Rating_St)
+            {
+                Output_Label.Text = "Millet selected. Rating of the crop type is " + Millet_Rating_St + ".";
+            }
+            else if (Is_Millet_Selected && Avg_NDVI < Millet_Rating_St)
+            {
+                Output_Label.Text = "Millet selected. Rating of the crop type is " + Millet_Rating_St + ". The crop type has a lower rating than the top rating " + Math.Round((Millet_Rating_St - Avg_NDVI) * 100, 2) + "%.";
+            }
+            if (Is_Sunflower_Selected && Avg_NDVI >= Sunflower_Rating_St)
+            {
+                Output_Label.Text = "Sunflower selected. Rating of the crop type is " + Sunflower_Rating_St + ".";
+            }
+            else if (Is_Sunflower_Selected && Avg_NDVI < Sunflower_Rating_St)
+            {
+                Output_Label.Text = "Sunflower selected. Rating of the crop type is " + Sunflower_Rating_St + ". The crop type has a lower rating than the top rating " + Math.Round((Sunflower_Rating_St - Avg_NDVI) * 100, 2) + "%.";
+            }
+            if (Is_Rice_Selected && Avg_NDVI >= Rice_Rating_St)
+            {
+                Output_Label.Text = "Rice selected. Rating of the crop type is " + Rice_Rating_St + ".";
+            }
+            else if (Is_Rice_Selected && Avg_NDVI < Rice_Rating_St)
+            {
+                Output_Label.Text = "Rice selected. Rating of the crop type is " + Rice_Rating_St + ". The crop type has a lower rating than the top rating " + Math.Round((Rice_Rating_St - Avg_NDVI) * 100, 2) + "%.";
+            }
+            Original_Image.Dispose();
+
+        }
+
+        private void Calculation_Of_INDEX(object sender, EventArgs e)
+        {
+            if (Picture_Box_Main1.Image == null)
+            {
+                MessageBox.Show("Please select an image.");
+                return;
+            }
+
+            All_Calculation(Image_Path);
+        }
+
+        private void Picture_Box_Main1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (Picture_Box_Main1.Image == null)
+            {
+                return;
+            }
+
+            Mouse_Down_Location = e.Location;
+            Is_Selected_Area = true;
+        }
+
+        private void Picture_Box_Main1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!Is_Selected_Area || Picture_Box_Main1.Image == null)
+            {
+                return;
+            }
+
+            Point mouseCurrentLocation = e.Location;
+            Selected_Area = new Rectangle(
+                Math.Min(Mouse_Down_Location.X, mouseCurrentLocation.X),
+                Math.Min(Mouse_Down_Location.Y, mouseCurrentLocation.Y),
+                Math.Abs(Mouse_Down_Location.X - mouseCurrentLocation.X),
+                Math.Abs(Mouse_Down_Location.Y - mouseCurrentLocation.Y)
+            );
+
+            using (Graphics g = Graphics.FromImage(Picture_Box_Main1.Image))
+            {
+                g.DrawRectangle(Pens.Blue, Selected_Area);
+            }
+
+            Picture_Box_Main1.Refresh();
+        }
+        private void Picture_Box_Main1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Is_Selected_Area = false;
+        }
+        private void Calculation_Start_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(Image_Path))
+            {
+                MessageBox.Show("Please select image first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            All_Calculation(Image_Path);
+        }
+
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (Is_Selected_Area && Picture_Box_Main1.Image != null)
+            {
+                using (Pen pen = new Pen(Color.Red))
+                {
+                    e.Graphics.DrawRectangle(pen, Selected_Area);
+                }
+            }
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            if (!Selected_Area.IsEmpty)
+            {
+
+                using (Brush brush = new SolidBrush(Color.FromArgb(128, Color.White)))
+                {
+                    e.Graphics.FillRectangle(brush, Selected_Area);
+                }
+            }
+        }
+        private void Picture_Box_Main1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
